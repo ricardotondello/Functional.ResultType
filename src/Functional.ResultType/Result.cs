ba8eeb242aad07cf3f6bdfcfde1f56e6349d3e39
Ricardo Tondello <ricardo.tondello@visma.com>
2023-09-14 09:17:58 +0200

    tryparse cast

--- src/Functional.ResultType/Result.cs
+++ src/Functional.ResultType/Result.cs
@@ -8,40 +8,35 @@ namespace Functional.ResultType;
 public record Result<T>(bool IsSuccess, T Value, IEnumerable<IReason>? Reasons)
 {
     private static readonly Result<T> FailDefaultResult = new(false, default!, Enumerable.Empty<IReason>());
+
+    private static readonly Result<T> FailDefaultResultTypeMismatch =
+        FailDefaultResult with { Reasons = new[] { Error.Create("Type mismatch") } };
+
     public Type Type => typeof(T);
     public bool HasErrors => GetErrors().Count > 0;
     public bool HasSuccesses => GetSuccesses().Count > 0;
     public static Result<T> Success(T value, IEnumerable<ISuccess>? successes = null) => new(true, value, successes);
     public static Result<T> Fail(T value, IEnumerable<IError>? errors = null) => new(false, value, errors);
-    public IReadOnlyList<IError> GetErrors() => new ReadOnlyCollection<IError>(Reasons?.OfType<IError>().ToArray() ?? Array.Empty<IError>());
-    public IReadOnlyList<ISuccess> GetSuccesses() => new ReadOnlyCollection<ISuccess>(Reasons?.OfType<ISuccess>().ToArray() ?? Array.Empty<ISuccess>());
+
+    public IReadOnlyList<IError> GetErrors() =>
+        new ReadOnlyCollection<IError>(Reasons?.OfType<IError>().ToArray() ?? Array.Empty<IError>());
+
+    public IReadOnlyList<ISuccess> GetSuccesses() =>
+        new ReadOnlyCollection<ISuccess>(Reasons?.OfType<ISuccess>().ToArray() ?? Array.Empty<ISuccess>());
+
     public static bool TryParse(object? obj, out Result<T> result)
     {
-        if (obj == null)
+        switch (obj)
         {
-            result = FailDefaultResult;
-            return false;
+            case null:
+                result = FailDefaultResult;
+                return false;
+            case T castedValue:
+                result = Success(castedValue);
+                return true;
+            default:
+                result = FailDefaultResultTypeMismatch;
+                return false;
         }
-
-        if (IsTypeMismatch(obj, out var mismatchResult))
-        {
-            result = mismatchResult!;
-            return false;
-        }
-
-        result = Success((T)obj);
-        return true;
-    }
-
-    private static bool IsTypeMismatch(object? obj, out Result<T>? result)
-    {
-        if (obj!.GetType() != typeof(T))
-        {
-            result = FailDefaultResult with { Reasons = new[] { Error.Create("Type mismatch") } };
-            return true;
-        }
-
-        result = null;
-        return false;
     }
 }
